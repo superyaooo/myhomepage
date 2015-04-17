3f7ebadb88a4a5092590d9f76fe3974cbb56c33a
superyaooo <superyaooo@gmail.com>
2015-04-17 00:44:16 +0000

    passenger_wsgi.py deleted online with Bitbucket

--- passenger_wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-INTERP = os.path.join(os.environ['HOME'], 'yaowangdev.com', 'bin', 'python')
-if sys.executebale != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
-sys.path.append(os.getcwd())
-
-
-sys.path.append('app')
-from app.app import app as application
