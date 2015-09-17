a4ecf5ed187eaf65799ad4e8e58895d44120e6a5
Yao Wang <yaowang@Yaos-MacBook-Pro.local>
2015-09-16 22:49:48 -0500

    updated brokentoystory url

--- app.py
+++ app.py
@@ -12,18 +12,13 @@ def index():
 @app.route('/resume', methods=['GET'])
 def resume():
     return render_template('resume.html')
-    
+
 @app.route('/projects', methods=['GET'])
 def projects():
     return render_template('projects.html')
 
 
-@app.route('/easychinesecooking', methods=['GET'])
-def easychinesecooking():
-    return render_template('easychinesecooking.html')
-
 
 
 if __name__ == '__main__':
     app.run()
-
